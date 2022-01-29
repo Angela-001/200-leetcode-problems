f404a985e39767033d6df475643b6a56712c008f
Vaibhav Shukla <52421094+mr-vaibh@users.noreply.github.com>
2022-01-30 02:15:38 +0530

    Create 13. Sqrt(x).py

---
+++ problems/13. Sqrt(x).py
@@ -0,0 +1,17 @@
+def mySqrt(x):
+    left_index = 0
+    right_index = x
+
+    while left_index < right_index:
+        mid = (left_index + right_index) // 2
+
+        if mid * mid <= x < (mid+1)*(mid+1):
+            return mid
+        if mid * mid > x:
+            right_index = mid
+        else:
+            left_index = mid + 1
+
+    return x
+
+print(mySqrt(67))
